37ecdd1852f076f4c6c9becece69110fbe4e23e0
BrianDX <boris.zaitsev@devexpress.com>
2018-08-02 18:16:51 +0300

    example content

---
+++ CS/WpfDashboardCustomTemplates/Dashboard1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WpfDashboardCustomTemplates
+{
+    partial class Dashboard1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard1));
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo1 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo2 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo3 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo4 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo5 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo6 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo7 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo8 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo9 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.ExcelWorksheetSettings excelWorksheetSettings1 = new DevExpress.DataAccess.Excel.ExcelWorksheetSettings();
+            DevExpress.DataAccess.Excel.ExcelSourceOptions excelSourceOptions1 = new DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1);
+            DevExpress.DashboardCommon.Dimension dimension1 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.GridDimensionColumn gridDimensionColumn1 = new DevExpress.DashboardCommon.GridDimensionColumn();
+            DevExpress.DashboardCommon.Measure measure1 = new DevExpress.DashboardCommon.Measure();
+            DevExpress.DashboardCommon.GridMeasureColumn gridMeasureColumn1 = new DevExpress.DashboardCommon.GridMeasureColumn();
+            DevExpress.DashboardCommon.Measure measure2 = new DevExpress.DashboardCommon.Measure();
+            DevExpress.DashboardCommon.GridMeasureColumn gridMeasureColumn2 = new DevExpress.DashboardCommon.GridMeasureColumn();
+            DevExpress.DashboardCommon.Measure measure3 = new DevExpress.DashboardCommon.Measure();
+            DevExpress.DashboardCommon.GridMeasureColumn gridMeasureColumn3 = new DevExpress.DashboardCommon.GridMeasureColumn();
+            DevExpress.DashboardCommon.Measure measure4 = new DevExpress.DashboardCommon.Measure();
+            DevExpress.DashboardCommon.GridMeasureColumn gridMeasureColumn4 = new DevExpress.DashboardCommon.GridMeasureColumn();
+            DevExpress.DashboardCommon.DashboardLayoutGroup dashboardLayoutGroup1 = new DevExpress.DashboardCommon.DashboardLayoutGroup();
+            DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem1 = new DevExpress.DashboardCommon.DashboardLayoutItem();
+            this.dashboardExcelDataSource1 = new DevExpress.DashboardCommon.DashboardExcelDataSource();
+            this.gridDashboardItem1 = new DevExpress.DashboardCommon.GridDashboardItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardExcelDataSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDashboardItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(measure1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(measure2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(measure3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(measure4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // dashboardExcelDataSource1
+            // 
+            this.dashboardExcelDataSource1.ComponentName = "dashboardExcelDataSource1";
+            this.dashboardExcelDataSource1.FileName = "|DataDirectory|\\Data\\Sales.xlsx";
+            this.dashboardExcelDataSource1.Name = "Excel Data Source 1";
+            this.dashboardExcelDataSource1.ResultSchemaSerializable = resources.GetString("dashboardExcelDataSource1.ResultSchemaSerializable");
+            fieldInfo1.Name = "Category";
+            fieldInfo1.Type = typeof(string);
+            fieldInfo2.Name = "Product";
+            fieldInfo2.Type = typeof(string);
+            fieldInfo3.Name = "State";
+            fieldInfo3.Type = typeof(string);
+            fieldInfo4.Name = "UnitsSoldYTD (Sum)";
+            fieldInfo4.Type = typeof(double);
+            fieldInfo5.Name = "UnitsSoldYTDTarget (Sum)";
+            fieldInfo5.Type = typeof(double);
+            fieldInfo6.Name = "RevenueQTD (Sum)";
+            fieldInfo6.Type = typeof(double);
+            fieldInfo7.Name = "RevenueQTDTarget (Sum)";
+            fieldInfo7.Type = typeof(double);
+            fieldInfo8.Name = "RevenueYTD (Sum)";
+            fieldInfo8.Type = typeof(double);
+            fieldInfo9.Name = "RevenueYTDTarget (Sum)";
+            fieldInfo9.Type = typeof(double);
+            this.dashboardExcelDataSource1.Schema.AddRange(new DevExpress.DataAccess.Excel.FieldInfo[] {
+            fieldInfo1,
+            fieldInfo2,
+            fieldInfo3,
+            fieldInfo4,
+            fieldInfo5,
+            fieldInfo6,
+            fieldInfo7,
+            fieldInfo8,
+            fieldInfo9});
+            excelWorksheetSettings1.CellRange = null;
+            excelWorksheetSettings1.WorksheetName = "Sheet1";
+            excelSourceOptions1.ImportSettings = excelWorksheetSettings1;
+            this.dashboardExcelDataSource1.SourceOptions = excelSourceOptions1;
+            // 
+            // gridDashboardItem1
+            // 
+            dimension1.DataMember = "Product";
+            gridDimensionColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
+            gridDimensionColumn1.AddDataItem("Dimension", dimension1);
+            measure1.DataMember = "RevenueQTD (Sum)";
+            gridMeasureColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
+            gridMeasureColumn1.AddDataItem("Measure", measure1);
+            measure2.DataMember = "RevenueQTDTarget (Sum)";
+            gridMeasureColumn2.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
+            gridMeasureColumn2.AddDataItem("Measure", measure2);
+            measure3.DataMember = "RevenueYTD (Sum)";
+            gridMeasureColumn3.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
+            gridMeasureColumn3.AddDataItem("Measure", measure3);
+            measure4.DataMember = "RevenueYTDTarget (Sum)";
+            gridMeasureColumn4.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
+            gridMeasureColumn4.AddDataItem("Measure", measure4);
+            this.gridDashboardItem1.Columns.AddRange(new DevExpress.DashboardCommon.GridColumnBase[] {
+            gridDimensionColumn1,
+            gridMeasureColumn1,
+            gridMeasureColumn2,
+            gridMeasureColumn3,
+            gridMeasureColumn4});
+            this.gridDashboardItem1.ComponentName = "gridDashboardItem1";
+            this.gridDashboardItem1.DataItemRepository.Clear();
+            this.gridDashboardItem1.DataItemRepository.Add(dimension1, "DataItem0");
+            this.gridDashboardItem1.DataItemRepository.Add(measure1, "DataItem1");
+            this.gridDashboardItem1.DataItemRepository.Add(measure2, "DataItem2");
+            this.gridDashboardItem1.DataItemRepository.Add(measure3, "DataItem3");
+            this.gridDashboardItem1.DataItemRepository.Add(measure4, "DataItem4");
+            this.gridDashboardItem1.DataSource = this.dashboardExcelDataSource1;
+            this.gridDashboardItem1.InteractivityOptions.IgnoreMasterFilters = false;
+            this.gridDashboardItem1.Name = "Grid 1";
+            this.gridDashboardItem1.ShowCaption = true;
+            // 
+            // Dashboard1
+            // 
+            this.DataSources.AddRange(new DevExpress.DashboardCommon.IDashboardDataSource[] {
+            this.dashboardExcelDataSource1});
+            this.Items.AddRange(new DevExpress.DashboardCommon.DashboardItem[] {
+            this.gridDashboardItem1});
+            dashboardLayoutItem1.DashboardItem = this.gridDashboardItem1;
+            dashboardLayoutItem1.Weight = 49.94753410283316D;
+            dashboardLayoutGroup1.ChildNodes.AddRange(new DevExpress.DashboardCommon.DashboardLayoutNode[] {
+            dashboardLayoutItem1});
+            dashboardLayoutGroup1.DashboardItem = null;
+            this.LayoutRoot = dashboardLayoutGroup1;
+            this.Title.Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardExcelDataSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(measure1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(measure2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(measure3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(measure4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDashboardItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardCommon.DashboardExcelDataSource dashboardExcelDataSource1;
+        private DevExpress.DashboardCommon.GridDashboardItem gridDashboardItem1;
+    }
+}
