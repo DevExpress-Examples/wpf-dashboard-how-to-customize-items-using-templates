1a5e886881e8c36934a224fa6a329bc4a2e9f3e9
DevExpressExampleBot <fakeEmail>
2022-12-18 22:58:25 +0400

    Source auto update [skip ci]

--- VB/WpfDashboardCustomTemplates/Dashboard1.Designer.vb
+++ VB/WpfDashboardCustomTemplates/Dashboard1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace WpfDashboardCustomTemplates
-    Partial Public Class Dashboard1
+Namespace WpfDashboardCustomTemplates
+
+    Partial Class Dashboard1
+
         ''' <summary> 
         ''' Required designer variable.
         ''' </summary>
@@ -10,53 +12,53 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Component Designer generated code"
-
+#Region "Component Designer generated code"
         ''' <summary> 
         ''' Required method for Designer support - do not modify 
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Dashboard1))
-            Dim fieldInfo1 As New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim fieldInfo2 As New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim fieldInfo3 As New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim fieldInfo4 As New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim fieldInfo5 As New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim fieldInfo6 As New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim fieldInfo7 As New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim fieldInfo8 As New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim fieldInfo9 As New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim excelWorksheetSettings1 As New DevExpress.DataAccess.Excel.ExcelWorksheetSettings()
-            Dim excelSourceOptions1 As New DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1)
-            Dim dimension1 As New DevExpress.DashboardCommon.Dimension()
-            Dim gridDimensionColumn1 As New DevExpress.DashboardCommon.GridDimensionColumn()
-            Dim measure1 As New DevExpress.DashboardCommon.Measure()
-            Dim gridMeasureColumn1 As New DevExpress.DashboardCommon.GridMeasureColumn()
-            Dim measure2 As New DevExpress.DashboardCommon.Measure()
-            Dim gridMeasureColumn2 As New DevExpress.DashboardCommon.GridMeasureColumn()
-            Dim measure3 As New DevExpress.DashboardCommon.Measure()
-            Dim gridMeasureColumn3 As New DevExpress.DashboardCommon.GridMeasureColumn()
-            Dim measure4 As New DevExpress.DashboardCommon.Measure()
-            Dim gridMeasureColumn4 As New DevExpress.DashboardCommon.GridMeasureColumn()
-            Dim dashboardLayoutGroup1 As New DevExpress.DashboardCommon.DashboardLayoutGroup()
-            Dim dashboardLayoutItem1 As New DevExpress.DashboardCommon.DashboardLayoutItem()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WpfDashboardCustomTemplates.Dashboard1))
+            Dim fieldInfo1 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo2 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo3 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo4 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo5 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo6 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo7 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo8 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo9 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim excelWorksheetSettings1 As DevExpress.DataAccess.Excel.ExcelWorksheetSettings = New DevExpress.DataAccess.Excel.ExcelWorksheetSettings()
+            Dim excelSourceOptions1 As DevExpress.DataAccess.Excel.ExcelSourceOptions = New DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1)
+            Dim dimension1 As DevExpress.DashboardCommon.Dimension = New DevExpress.DashboardCommon.Dimension()
+            Dim gridDimensionColumn1 As DevExpress.DashboardCommon.GridDimensionColumn = New DevExpress.DashboardCommon.GridDimensionColumn()
+            Dim measure1 As DevExpress.DashboardCommon.Measure = New DevExpress.DashboardCommon.Measure()
+            Dim gridMeasureColumn1 As DevExpress.DashboardCommon.GridMeasureColumn = New DevExpress.DashboardCommon.GridMeasureColumn()
+            Dim measure2 As DevExpress.DashboardCommon.Measure = New DevExpress.DashboardCommon.Measure()
+            Dim gridMeasureColumn2 As DevExpress.DashboardCommon.GridMeasureColumn = New DevExpress.DashboardCommon.GridMeasureColumn()
+            Dim measure3 As DevExpress.DashboardCommon.Measure = New DevExpress.DashboardCommon.Measure()
+            Dim gridMeasureColumn3 As DevExpress.DashboardCommon.GridMeasureColumn = New DevExpress.DashboardCommon.GridMeasureColumn()
+            Dim measure4 As DevExpress.DashboardCommon.Measure = New DevExpress.DashboardCommon.Measure()
+            Dim gridMeasureColumn4 As DevExpress.DashboardCommon.GridMeasureColumn = New DevExpress.DashboardCommon.GridMeasureColumn()
+            Dim dashboardLayoutGroup1 As DevExpress.DashboardCommon.DashboardLayoutGroup = New DevExpress.DashboardCommon.DashboardLayoutGroup()
+            Dim dashboardLayoutItem1 As DevExpress.DashboardCommon.DashboardLayoutItem = New DevExpress.DashboardCommon.DashboardLayoutItem()
             Me.dashboardExcelDataSource1 = New DevExpress.DashboardCommon.DashboardExcelDataSource()
             Me.gridDashboardItem1 = New DevExpress.DashboardCommon.GridDashboardItem()
-            CType(Me.dashboardExcelDataSource1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.gridDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(measure1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(measure2, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(measure3, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(measure4, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dashboardExcelDataSource1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridDashboardItem1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((dimension1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((measure1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((measure2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((measure3), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((measure4), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).BeginInit()
             ' 
             ' dashboardExcelDataSource1
             ' 
@@ -82,7 +84,7 @@
             fieldInfo8.Type = GetType(Double)
             fieldInfo9.Name = "RevenueYTDTarget (Sum)"
             fieldInfo9.Type = GetType(Double)
-            Me.dashboardExcelDataSource1.Schema.AddRange(New DevExpress.DataAccess.Excel.FieldInfo() { fieldInfo1, fieldInfo2, fieldInfo3, fieldInfo4, fieldInfo5, fieldInfo6, fieldInfo7, fieldInfo8, fieldInfo9})
+            Me.dashboardExcelDataSource1.Schema.AddRange(New DevExpress.DataAccess.Excel.FieldInfo() {fieldInfo1, fieldInfo2, fieldInfo3, fieldInfo4, fieldInfo5, fieldInfo6, fieldInfo7, fieldInfo8, fieldInfo9})
             excelWorksheetSettings1.CellRange = Nothing
             excelWorksheetSettings1.WorksheetName = "Sheet1"
             excelSourceOptions1.ImportSettings = excelWorksheetSettings1
@@ -105,7 +107,7 @@
             measure4.DataMember = "RevenueYTDTarget (Sum)"
             gridMeasureColumn4.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight
             gridMeasureColumn4.AddDataItem("Measure", measure4)
-            Me.gridDashboardItem1.Columns.AddRange(New DevExpress.DashboardCommon.GridColumnBase() { gridDimensionColumn1, gridMeasureColumn1, gridMeasureColumn2, gridMeasureColumn3, gridMeasureColumn4})
+            Me.gridDashboardItem1.Columns.AddRange(New DevExpress.DashboardCommon.GridColumnBase() {gridDimensionColumn1, gridMeasureColumn1, gridMeasureColumn2, gridMeasureColumn3, gridMeasureColumn4})
             Me.gridDashboardItem1.ComponentName = "gridDashboardItem1"
             Me.gridDashboardItem1.DataItemRepository.Clear()
             Me.gridDashboardItem1.DataItemRepository.Add(dimension1, "DataItem0")
@@ -120,28 +122,27 @@
             ' 
             ' Dashboard1
             ' 
-            Me.DataSources.AddRange(New DevExpress.DashboardCommon.IDashboardDataSource() { Me.dashboardExcelDataSource1})
-            Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() { Me.gridDashboardItem1})
+            Me.DataSources.AddRange(New DevExpress.DashboardCommon.IDashboardDataSource() {Me.dashboardExcelDataSource1})
+            Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() {Me.gridDashboardItem1})
             dashboardLayoutItem1.DashboardItem = Me.gridDashboardItem1
             dashboardLayoutItem1.Weight = 49.94753410283316R
-            dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutItem1})
+            dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() {dashboardLayoutItem1})
             dashboardLayoutGroup1.DashboardItem = Nothing
             Me.LayoutRoot = dashboardLayoutGroup1
             Me.Title.Text = "Dashboard"
-            CType(Me.dashboardExcelDataSource1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(dimension1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(measure1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(measure2, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(measure3, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(measure4, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.gridDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
-
+            CType((Me.dashboardExcelDataSource1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((dimension1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((measure1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((measure2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((measure3), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((measure4), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridDashboardItem1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).EndInit()
         End Sub
 
-        #End Region
-
+#End Region
         Private dashboardExcelDataSource1 As DevExpress.DashboardCommon.DashboardExcelDataSource
+
         Private gridDashboardItem1 As DevExpress.DashboardCommon.GridDashboardItem
     End Class
 End Namespace
