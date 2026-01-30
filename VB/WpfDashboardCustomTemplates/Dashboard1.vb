486b1a43633f2eee8e94a7e0d148d697386bdd2e
DevExpressExampleBot <fakeEmail>
2026-01-30 12:50:00 +0400

    autogenerate VB

--- VB/WpfDashboardCustomTemplates/Dashboard1.vb
+++ VB/WpfDashboardCustomTemplates/Dashboard1.vb
@@ -1,17 +1,12 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports DevExpress.DataAccess
 Imports DevExpress.DashboardCommon
 
 Namespace WpfDashboardCustomTemplates
-	Partial Public Class Dashboard1
-		Inherits DevExpress.DashboardCommon.Dashboard
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class Dashboard1
+        Inherits Dashboard
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 End Namespace
